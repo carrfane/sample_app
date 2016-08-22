94c16676098aa1f513032ccc21310fc7e3b36272
Hector Carrillo <carrfane@gmail.com>
2016-08-22 15:40:20 -0400

    Add Password reset

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-	attr_accessor :remember_token, :activation_token
+	attr_accessor :remember_token, :activation_token, :reset_token
   before_save   :downcase_email
   before_create :create_activation_digest
 	validates :name, presence: true, length: { maximum: 50 }
@@ -14,36 +14,52 @@ class User < ApplicationRecord
       cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                     BCrypt::Engine.cost
       BCrypt::Password.create(string, cost: cost)
-    end
+  end
 
-    def User.new_token
-      SecureRandom.urlsafe_base64
-    end
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
 
-    def remember
+  def remember
       self.remember_token = User.new_token
       update_attribute(:remember_digest, User.digest(remember_token))
-    end
+  end
 
-    def authenticated?(attribute, token)
-      digest = send("#{attribute}_digest")
-      return false if digest.nil?
-      BCrypt::Password.new(digest).is_password?(token)
-    end
-    def forget
-      update_attribute(:remember_digest, nil)
-    end
+  def authenticated?(attribute, token)
+    digest = send("#{attribute}_digest")
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
+  end
+    
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
 
-    def activate
-      update_columns(activated: true, activated_at: Time.zone.now)
-      #update_attribute(:activated,    true)
-      #update_attribute(:activated_at, Time.zone.now)
-    end
+  def activate
+    update_columns(activated: true, activated_at: Time.zone.now)
+  end
 
-    # Sends activation email.
-    def send_activation_email
-      UserMailer.account_activation(self).deliver_now
-    end
+  # Sends activation email.
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
+  # Sets the password reset attributes.
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_columns(reset_digest: User.digest(reset_token), reset_sent_at: Time.zone.now)
+    #update_attribute(:reset_digest,  User.digest(reset_token))
+    #update_attribute(:reset_sent_at, Time.zone.now)
+  end
+
+  # Sends password reset email.
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
+  end
+
   private
     # Converts email to all lower-case.
     def downcase_email
--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,13 @@
 Rails.application.routes.draw do
-  get '/login', to: 'sessions#new'
-
-  post '/login', to: 'sessions#create'
-  
-  delete 'logout', to: 'sessions#destroy'
-
+  root   'static_pages#home'
+  get    '/help',    to: 'static_pages#help'
+  get    '/about',   to: 'static_pages#about'
+  get    '/contact', to: 'static_pages#contact'
+  get    '/signup',  to: 'users#new'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   resources :users
-
   resources :account_activations, only: [:edit]
-  
-  post '/signup',  to: 'users#create'
-  
-  get '/signup', to: 'users#new'
-
-  get '/home', to: 'static_pages#home'
-
-  get  '/help', to: 'static_pages#help'
-
-  get '/about', to: 'static_pages#about'
-
-  get '/contact', to: 'static_pages#contact'
-
-  root 'static_pages#home'
+  resources :password_resets,     only: [:new, :create, :edit, :update]
 end
